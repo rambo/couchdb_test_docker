422a7596487bf5dbbf5ed21b1d95917af72a3393
Eero af Heurlin <eero.afheurlin@iki.fi>
2018-12-13 16:01:08 +0200

    client setup script seems to work now

--- client_docker_setup.sh
+++ client_docker_setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 if [ -z "${CLIENTNAME}" ]
 then
   echo "CLIENTNAME must be defined"
@@ -36,6 +36,8 @@ docker exec -it $NODENAME /bin/bash -c 'CHECK=1; while [ $CHECK -ne 0 ]; do curl
 docker exec -it $NODENAME /usr/bin/curl -X PUT http://127.0.0.1:5984/_node/_local/_config/admins/admin -d '"'$CLIENTADMINPW'"'
 docker exec -it $NODENAME /usr/bin/curl -X PUT http://admin:$CLIENTADMINPW@127.0.0.1:5984/_node/_local/_config/couch_httpd_auth/secret -d '"'$HTTP_SECRET'"'
 docker exec -it $NODENAME /usr/bin/curl -X PUT http://admin:$CLIENTADMINPW@127.0.0.1:5984/_node/_local/_config/admins/replicator -d '"'$REPLICATORPW'"'
+docker exec -it $NODENAME /usr/bin/curl -X POST -H "Content-Type: application/json" http://admin:$CLIENTADMINPW@127.0.0.1:5984/_cluster_setup -d '{"action": "finish_cluster"}'
+
 
 docker exec -it $CLUSTERCOORDNODE /usr/bin/curl -X PUT http://admin:$CLUSTERADMINPW@127.0.0.1:5984/_users/org.couchdb.user:$CLIENTUSER -H "Content-Type: application/json" -d '{"name": "'$CLIENTUSER'", "password": "'$REPLICATORPW'", "roles": [], "type": "user"}'
 echo "*** ACTION: On the cluster add user $CLIENTUSER as member to vaplatformdata"
@@ -43,7 +45,9 @@ echo "*** ACTION: On the cluster add user $CLIENTUSER as member to vaplatformdat
 
 NODEIP=`docker inspect -f '{{.NetworkSettings.Networks.fake_internet.IPAddress}}' $NODENAME`
 
+docker exec -it $NODENAME /usr/bin/curl -X PUT -H "Content-Type: application/json" http://admin:$CLIENTADMINPW@127.0.0.1:5984/vaplatformdata
+sleep 5
 docker exec -it $CLUSTERCOORDNODE /usr/bin/curl -X POST http://admin:$CLUSTERADMINPW@127.0.0.1:5984/_replicator -H "Content-Type: application/json" -d '{ "_id": "'$CLIENTNAME'_vaplatform_downstream", "source": "http://'$CLIENTUSER':'$REPLICATORPW'@127.0.0.1:5984/vaplatformdata", "target": "http://replicator:'$REPLICATORPW'@'$NODEIP':5984/vaplatformdata", "selector": { "clientid": "'$CLIENTNAME'" }, "continuous":  true}'
-docker exec -it $NODENAME /usr/bin/curl -X POST http://admin:$CLIENTADMINPW@127.0.0.1:5984/_replicator -H "Content-Type: application/json" -d '{ "_id": "'$CLIENTNAME'_vaplatform_upstream", "source": "http://replicator:'$REPLICATORPW'@127.0.0.1:5984/vaplatformdata", "target": "http://'$CLIENTUSER':'$REPLICATORPW'@'$CLUSTERCOORDNODEIP':5984/vaplatformdata", "selector": { "clientid": "'$CLIENTNAME'" }, "continuous":  true, "create_target":  true}'
+docker exec -it $NODENAME /usr/bin/curl -X POST http://admin:$CLIENTADMINPW@127.0.0.1:5984/_replicator -H "Content-Type: application/json" -d '{ "_id": "'$CLIENTNAME'_vaplatform_upstream", "source": "http://replicator:'$REPLICATORPW'@127.0.0.1:5984/vaplatformdata", "target": "http://'$CLIENTUSER':'$REPLICATORPW'@'$CLUSTERCOORDNODEIP':5984/vaplatformdata", "selector": { "clientid": "'$CLIENTNAME'" }, "continuous":  true}'
 
 echo "*** $NODENAME admin pw is $CLIENTADMINPW replicator pw is $REPLICATORPW ***"
